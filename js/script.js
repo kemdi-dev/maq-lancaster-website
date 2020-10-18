18b7f794caaa359a876e388b69691f58ea0f79f3
kemdi <49282862+kemdi2019@users.noreply.github.com>
2020-10-18 00:51:21 -0400

    updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.main-links')
+    const navLinks = document.querySelectorAll('.main-links li');
+    
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });
+
+    
+}
+
+navSlide();
+
+
+
 //navbar disappearing
 //Reference: https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
 var prevScrollpos = window.pageYOffset;
